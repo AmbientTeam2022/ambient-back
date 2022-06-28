d7b67fbdd1fe96f64312e54dc8594370ec5719ff
MisaoDev <69985862+MisaoDev@users.noreply.github.com>
2022-06-27 21:24:27 -0400

    documenta middlewares

--- src/api/middleware/allowCors.js
+++ src/api/middleware/allowCors.js
@@ -1,3 +1,18 @@
+/**
+ * @module allowCors
+ * @category middleware
+ */
+
+/**
+ * Permite a esta ruta ignorar la restricción de CORS
+ *
+ * Modifica la cabecera de la solicitud, agregándole las cabeceras necesarias
+ * para que parezca que viene del origen que corresponde. Permitiendo así que
+ * pase el filtro de CORS (Cross-Origin Resource Sharing).
+ * @param {Object} req Solicitud
+ * @param {Object} res Respuesta
+ * @param {Function} next Siguiente middleware o controller
+ */
 const allowCors = function (req, res, next) {
   const origin =
     process.env.NODE_ENV === 'dev'
--- src/api/middleware/authRequired.js
+++ src/api/middleware/authRequired.js
@@ -1,5 +1,23 @@
+/**
+ * @module authRequired
+ * @category middleware
+ */
+
 const jwt = require('jsonwebtoken')
 
+/**
+ * Exige autorización en esta ruta
+ *
+ * Verifica que la ruta venga con un Token de autorización JWT (_Bearer Token_).
+ * El Token debe venir en la cabezera `Authorization` de la solicitud.
+ *
+ * Si no se envía un Token, es inválido o ha expirado, devuelve un mensaje de error.
+ * Caso contrario agrega a la solicitud los datos del usuario encriptados en el Token,
+ * para luego continuar con la ruta.
+ * @param {Object} req Solicitud
+ * @param {Object} res Respuesta
+ * @param {Function} next Siguiente middleware o controller
+ */
 function authRequired(req, res, next) {
   const authHeader = req.header('Authorization')
   const token = authHeader?.split(' ')?.[1]
